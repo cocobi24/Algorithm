0efcabf171e6e4cad15c2bd5f34723f0f465b6dc
seonhui-lee <cacabi24@hanmail.net>
2022-11-26 21:09:45 +0900

    Silver3_1003_피보나치 함수
    Silver3_1003_피보나치 함수

---
+++ silver/BOJ1003.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/1003
+
+def fibo(n) :
+    if n == 0 :
+        return 0
+    if n == 1 :
+        return 1
+    if dp[n] :
+        return dp[n]
+    dp[n] = fibo(n-1) + fibo(n -2)
+    return dp[n]
+
+dp = [0] * 41
+t = int(input())
+for _ in range(t) :
+    n = int(input())
+    if n == 0 :
+        print(1,0)
+    elif n == 1:
+        print(0, 1)
+    else:
+        print(fibo(n-1), fibo(n))
