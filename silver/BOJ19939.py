98862ba856deb8ac9413a332eb29e6870f2477ad
hojin <cacabi24@hanmail.net>
2023-03-13 23:02:39 +0900

    Silver4_19939_박 터뜨리기
    Silver4_19939_박 터뜨리기

---
+++ silver/BOJ19939.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/19939
+
+n, k = map(int, input().split())
+basket = k * (k + 1) // 2
+
+if n < basket:
+    print(-1)
+else:
+    if (n - basket) % k == 0:
+        print(k - 1)
+    else:
+        print(k)
