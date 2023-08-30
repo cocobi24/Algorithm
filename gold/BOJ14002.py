207d0d877c6ba693f7403f45c2b77ca9c6a945ba
Jinny <73522666+cocobi24@users.noreply.github.com>
2023-08-30 16:46:03 +0900

    Gold4_14002_가장 긴 증가하는 부분 수열 4
    Gold4_14002_가장 긴 증가하는 부분 수열 4

---
+++ gold/BOJ14002.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/14002
+
+from bisect import bisect_left
+n, nums = int(input()), list(map(int, input().split()))
+
+lis, bis = [], []
+for num in nums:
+    if not lis or lis[-1] < num:
+        lis.append(num)
+        bis.append((len(lis)-1, num))
+    else:
+        lis[bisect_left(lis, num)] = num
+        bis.append((bisect_left(lis, num), num))
+print(len(lis))
+
+ans, start, end = [], len(bis)-1, len(lis)-1
+for i in range(start, -1, -1):
+    if bis[i][0] == end:
+        ans.append(bis[i][1])
+        end -= 1
+print(*ans[::-1])
