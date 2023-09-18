d1e664a51eb358523483e1ce4905355434eea6c0
Jinny <cacabi24@hanmail.net>
2023-09-18 16:16:34 +0900

    Bronze3_2522_별 찍기 - 12
    Bronze3_2522_별 찍기 - 12

---
+++ bronze/BOJ2522.py
@@ -0,0 +1,7 @@
+# https://www.acmicpc.net/problem/2522
+
+n = int(input())
+for i in range(n, 0, -1):
+    print(' ' * (i-1), '*'*(n+1-i), sep="")
+for i in range(1, n):
+    print(' ' * i, '*' * (n-i), sep="")
