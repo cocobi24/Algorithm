fffc60abc41744363c467f1ecbacaaf55ca047db
Jinny <cacabi24@hanmail.net>
2023-10-19 11:05:16 +0900

    Bronze1_11285_초성 중성 종성2
    Bronze1_11285_초성 중성 종성2

---
+++ bronze/BOJ11285.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/11285
+
+import sys
+input = sys.stdin.readline
+first = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+middle = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+last = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+word = list(input().strip() for i in range(3))
+a, b, c = first.index(word[0]), middle.index(word[1]), last.index(word[2])
+answer = chr(((a * 21) + b) * 28 + c + ord('가'))
+print(answer)
