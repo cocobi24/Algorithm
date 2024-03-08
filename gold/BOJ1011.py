f8380306d67c3be01c22c12a70739722ec4279e2
Jinny <cacabi24@hanmail.net>
2024-03-08 10:09:15 +0900

    Gold5_1011_Fly me to the Alpha Centauri
    Gold5_1011_Fly me to the Alpha Centauri

---
+++ gold/BOJ1011.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/1011
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+  X, Y = map(int, input().split())
+  ans = 0
+
+  if X != Y:
+    n = int((Y - X)**0.5)
+    m = (Y - X) - n**2
+    if n**2 == Y - X:
+      ans = 2 * n - 1
+    else:
+      ans = (2 * n) if m <= n else (2 * n + 1)
+  print(ans)
