8df2dda407697bc703502954ce2121f107942d10
Jinny <cacabi24@hanmail.net>
2024-04-19 14:54:23 +0900

    Gold5_2467_용액
    Gold5_2467_용액

---
+++ gold/BOJ2467.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/2467
+
+n = int(input())
+values = sorted((abs(int(elem)), int(elem)) for elem in input().split())
+answer, prev, max_value = [], values[0][1], 2000000001
+for i in range(1, n):
+  value = values[i][1]
+  cur = abs(prev + value)
+
+  if cur < max_value:
+      max_value = cur
+      answer = [prev, value]
+  prev = value
+
+answer.sort()
+print(*answer)
