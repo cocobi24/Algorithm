766ee10c85f78e977a246da88fb95c3422270c91
Jinny <cacabi24@hanmail.net>
2024-04-23 19:34:23 +0900

    Bronze5_28235_코드마스터 2023
    Bronze5_28235_코드마스터 2023

---
+++ bronze/BOJ28235.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/28235
+
+fighting = {
+  "SONGDO": "HIGHSCHOOL",
+  "CODE": "MASTER",
+  "2023": "0611",
+  "ALGORITHM": "CONTEST",
+}
+print(fighting[input().rstrip()])
